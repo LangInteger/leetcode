75d9ffee565b2d2cdcc55d04ba08696db292d16f
lang <709091988@qq.com>
2022-04-08 02:37:04 +0800

    feat - add NonRepeatSubStringLength

---
+++ src/com/company/exercise/NonRepeatSubStringLength.java
@@ -0,0 +1,45 @@
+package com.company.exercise;
+
+import java.util.*;
+
+public class NonRepeatSubStringLength {
+    public static void main(String[] args) {
+        NonRepeatSubStringLength procedure = new NonRepeatSubStringLength();
+        procedure.lengthOfLongestSubstring("abcabcbb");
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+
+        Map<Character, Integer> indexMapByChar = new HashMap<>();
+        int curStartIndex = 0;
+        int maxLen = 0;
+        for (int index = 0; index < s.length(); index++) {
+            if (indexMapByChar.containsKey(s.charAt(index))) {
+                maxLen = getMaxLen(maxLen, curStartIndex, index);
+                curStartIndex = indexMapByChar.get(s.charAt(index)) + 1;
+
+                indexMapByChar = new HashMap<>();
+                for (int i = curStartIndex; i < index; i++) {
+                    indexMapByChar.put(s.charAt(i), i);
+                }
+            } else {
+                if (index == s.length() - 1) {
+                    maxLen = getMaxLen(maxLen, curStartIndex, index + 1);
+                }
+            }
+
+            indexMapByChar.put(s.charAt(index), index);
+        }
+
+        return maxLen;
+    }
+
+    private int getMaxLen(int maxLen, int curStartIndex, int index) {
+        int curMaxLen = index - curStartIndex;
+
+        if (curMaxLen > maxLen) {
+            maxLen = curMaxLen;
+        }
+        return maxLen;
+    }
+}
